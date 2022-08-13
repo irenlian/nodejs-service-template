c2afa7b7c43db30483643b0f9ec251ac0c3c0b8f
Dmytro Lian <lyandima2011@gmail.com>
2022-08-13 17:40:33 +0300

    rename SERVER_PORT env var to PORT

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,4 @@ export const dbPoolMin = parseInt(process.env.DB_POOL_MIN || '', 10) || 2;
 export const dbPoolMax = parseInt(process.env.DB_POOL_MAX || '', 10) || 30;
 
 export const environment = process.env.ENVIRONMENT || 'development';
-export const port = parseInt(process.env.SERVER_PORT || '', 10) || 4000;
+export const port = parseInt(process.env.PORT || '', 10) || 4000;
